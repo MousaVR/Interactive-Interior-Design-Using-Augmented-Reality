967069c21ff263d5c8d4e46ae7fb684a8da4a3f9
mahmoud mousa <mahmoudmostafa194@gmail.com>
2020-05-07 22:35:16 +0200

    changes in AR Core

---
+++ Assets/_Content/_Scripts/ARController.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+#if UNITY_EDITOR
+// Set up touch input propagation while using Instant Preview in the editor.
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class ARController : MonoBehaviour
+{
+    public GameObject AssuranceCanvus;
+    public Camera FirstPersonCamera;
+    public GameObject Gate;
+    public float GateHieght = .25f;
+    public FollowObject modelToShow;
+    private bool m_IsQuitting = false;
+    private Vector3 hitPosition;
+    private Quaternion hitRotation;
+
+    public void Awake()
+    {
+        // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+        Application.targetFrameRate = 60;
+    }
+
+    private void Start()
+    {
+        AssuranceCanvus.SetActive(false);
+        Gate.SetActive(false);
+    }
+
+    void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        // If the player has not touched the screen, we are done with this update.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Should not handle input if the player is pointing on UI.
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                // Choose the prefab based on the Trackable that got hit.
+                GameObject prefab;
+                if (hit.Trackable is DetectedPlane)
+                {
+                    DetectedPlane detectedPlane = hit.Trackable as DetectedPlane;
+                    if (detectedPlane.PlaneType == DetectedPlaneType.HorizontalUpwardFacing)
+                    {
+                        if (!Gate.activeInHierarchy)
+                        {
+                            Gate.SetActive(true);
+                        }
+                        hitPosition = new Vector3(hit.Pose.position.x, hit.Pose.position.y + GateHieght, hit.Pose.position.z);
+                        Gate.transform.position= hitPosition;
+                        hitRotation = hit.Pose.rotation;
+                        Gate.transform.rotation = hitRotation;
+                        //prefab.transform.Rotate(0, 0, 0, Space.Self);
+                        var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                        Gate.transform.parent = anchor.transform;
+                        AssuranceCanvus.SetActive(true);
+                        modelToShow.GetComponent<FollowObject>().FollowTheObject(hitPosition, hitRotation);
+                    }
+                }
+            }
+        }
+
+
+    }
+        #region life cycle
+        private void _UpdateApplicationLifecycle()
+        {
+            // Exit the app when the 'back' button is pressed.
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            // Only allow the screen to sleep when not tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                Screen.sleepTimeout = SleepTimeout.SystemSetting;
+            }
+            else
+            {
+                Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            }
+
+            if (m_IsQuitting)
+            {
+                return;
+            }
+
+            // Quit if ARCore was unable to connect and give Unity some time for the toast to
+            // appear.
+            if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+            {
+                _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+            else if (Session.Status.IsError())
+            {
+                _ShowAndroidToastMessage(
+                    "ARCore encountered a problem connecting.  Please start the app again.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+        }
+
+        /// <summary>
+        /// Actually quit the application.
+        /// </summary>
+        private void _DoQuit()
+        {
+            Application.Quit();
+        }
+
+        /// <param name="message">Message string to show in the toast.</param>
+        private void _ShowAndroidToastMessage(string message)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity =
+                unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityActivity != null)
+            {
+                AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+                unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                {
+                    AndroidJavaObject toastObject =
+                        toastClass.CallStatic<AndroidJavaObject>(
+                            "makeText", unityActivity, message, 0);
+                    toastObject.Call("show");
+                }));
+            }
+        }
+        #endregion
+}
---
+++ Assets/_Content/_Scripts/FollowObject.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+
+public class FollowObject : MonoBehaviour
+{
+
+    public void FollowTheObject(Vector3 pos, Quaternion rot)
+    {
+        transform.position = pos;
+        transform.rotation = rot;
+    }
+}
